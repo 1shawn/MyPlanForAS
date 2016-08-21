ed2b31f208d4d0226608aa07bb29c537af5e66b7
zhouf <578751753@qq.com>
2016-08-21 10:01:15 +0800

    消息中心代码

--- app/src/main/java/com/andwho/myplan/activity/MsgCenter.java
+++ app/src/main/java/com/andwho/myplan/activity/MsgCenter.java
@@ -1,7 +1,55 @@
 package com.andwho.myplan.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.andwho.myplan.R;
+import com.andwho.myplan.view.CustomEditView;
+
 /**
  * Created by zhouf on 16/4/24.
+ * 消息中心
  */
-public class MsgCenter {
+public class MsgCenter extends BaseAct implements View.OnClickListener {
+
+    private CustomEditView mNewPswView;
+
+    private CustomEditView mPswView;
+
+    private CustomEditView mNewPswAginView;
+
+    private Button mChangeBtn;
+    private Activity myselfContext;
+    private LinearLayout ll_leftIcon, ll_life;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forget_psw);
+
+        myselfContext = this;
+        findViews();
+    }
+    private void findViews() {
+        findViewById(R.id.login_user_icon_iv).setVisibility(View.GONE);
+        mPswView = (CustomEditView) findViewById(R.id.login_psw_et);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_login:
+                break;
+            case R.id.ll_leftIcon:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/andwho/myplan/contentprovider/MyPlanMsgHelp.java
+++ app/src/main/java/com/andwho/myplan/contentprovider/MyPlanMsgHelp.java
@@ -1,7 +1,42 @@
 package com.andwho.myplan.contentprovider;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by zhouf on 16/4/24.
  */
-public class MyPlanMsgHelp {
+public class MyPlanMsgHelp extends SQLiteOpenHelper {
+
+    // 数据库名
+    public static final String DB_NAME = "myplan_provider.db";// 数据库名
+    // 表名
+    public static final String TABLE_NAME = "myplanMsgCenter";// 表名
+    // 表字段
+    public static final String ID = "_id";// 表键
+    public static final String TITILE = "title";// 标题
+    public static final String CONTENT = "content";// 内容
+    public static final String AUTHORITY = "com.andwho.myplanprovider";//
+    // URL
+    public static final String CONTENT_URI = "content://" + AUTHORITY + "/"
+            + TABLE_NAME;
+
+    public MyPlanMsgHelp(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(" + ID
+                + " integer primary key autoincrement," + TITILE + " TEXT,"
+                + CONTENT +  " TEXT)");
+
+    }
+
+    // 当数据库版本号发生变化时调用该方法
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+
+    }
 }
